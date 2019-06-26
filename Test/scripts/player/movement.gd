8fdad7aaf2ce64354f2d8f855817a41e6d95282e
Brendan Weirtz <brendanweirtz@gmail.com>
2019-06-26 23:46:42 +0000

    Replace movement.gd

--- Test/scripts/player/movement.gd
+++ Test/scripts/player/movement.gd
@@ -3,14 +3,15 @@ class_name Movement
 
 #--------------------
 # General Variables
-var velocity = Vector3()
-var direction = Vector3()
+var velocity : = Vector3()
+var direction : = Vector3()
 
 #--------------------
 # Camera
 var tween
 var fovDefault : = 64
 var fovRunning : = 68
+var playerCamera = get_owner().get_node("PlayerCamera")
 #--------------------
 # Walking
 var gravity : = -9.8 * 2
@@ -22,25 +23,25 @@ var speed = MAX_SPEED
 #--------------------
 # Movement
 const JUMP_HEIGHT : = 6
-var crouch
-var on_floor
+var on_floor : bool
+var playerRayCast = get_owner().get_node("RayCast")
 var rayCast : = false
 var running : = false
 var standing : = false
 #--------------------
 # Slope
-const MAX_SLOPE_ANGLE = 10
+const MAX_SLOPE_ANGLE : = 10
 
 func _ready():
-	on_floor = $"../../../KinematicBody".is_on_floor()
+	on_floor = get_owner().is_on_floor()
 
 func walk(delta):
 	#Reset the direction of the player
 	direction = Vector3()
 	#Get rotation of camera
-	var aim = $"../../PlayerCamera".get_global_transform().basis
+	var aim = playerCamera.get_global_transform().basis
 	#Grab tween node
-	tween = $"../../PlayerCamera/FovTween"
+	tween = playerCamera.get_node("FovTween")
 	
 	# MOVEMENT INPUT [W,A,S,D] ------------------------------------------------------------
 	if(Input.is_action_pressed("move_fw")):
@@ -48,12 +49,12 @@ func walk(delta):
 		standing = false
 		if Input.is_action_just_pressed("move_sprint"):
 			speed = MAX_RUNNING_SPEED
-			tween.interpolate_property($"../../PlayerCamera", "fov", $"../../PlayerCamera".fov, fovRunning, 0.1, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+			tween.interpolate_property(playerCamera, "fov", playerCamera.fov, fovRunning, 0.1, Tween.TRANS_LINEAR, Tween.EASE_OUT)
 			tween.start()
 			running = true
 	if Input.is_action_just_released("move_fw"):
 		speed = MAX_SPEED
-		tween.interpolate_property($"../../PlayerCamera", "fov", $"../../PlayerCamera".fov, fovDefault, 0.1, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+		tween.interpolate_property(playerCamera, "fov", playerCamera.fov, fovDefault, 0.1, Tween.TRANS_LINEAR, Tween.EASE_OUT)
 		tween.start()
 		running = false
 		standing = true
@@ -76,26 +77,27 @@ func walk(delta):
 	direction = direction.normalized()
 	
 	#Double Verification if on floor, handles walking on slopes (RayCast)
-	if $"../../../KinematicBody".is_on_floor():
+	if get_owner().is_on_floor():
 		rayCast = true
-		on_floor = $"../../../KinematicBody".is_on_floor()
-		var n = $"../../RayCast".get_collision_normal()
+		on_floor = get_owner().is_on_floor()
+		var n = playerRayCast.get_collision_normal()
 		var floor_angle = rad2deg(acos(n.dot(Vector3(0,1,0))))
 		if floor_angle > MAX_SLOPE_ANGLE:
 			velocity.y += (gravity + (gravity * floor_angle) / 5) * delta
 		print("KinematicBody is_on_floor")
 	else:
-		if !$"../../RayCast".is_colliding(): # if raycast is NOT colliding
+		if !playerRayCast.is_colliding(): # if raycast is NOT colliding
 			rayCast = false # raycast = false
-			on_floor = $"../../../KinematicBody".is_on_floor()
+			on_floor = get_owner().is_on_floor()
 			print("RayCast NOT COLLIDING")
 		#CREATES ERROR WITH IS_ON_FLOOR_VAR_FLOPPING \/
+		#Slope skipping -> relocate move and slide when gravity is applied.
 		velocity.y += gravity * delta #removing makes is on floor false, but keeping it makes it flipflop
 		print("KinematicBody NOT ON FLOOR")
 		
-	if rayCast and !$"../../../KinematicBody".is_on_floor():
+	if rayCast and !get_owner().is_on_floor():
 		print("RayCast Colliding and NOT is_on_floor (MOVING PLAYER DOWN)")
-		$"../../../KinematicBody".move_and_collide(Vector3(0,-1, 0))
+		get_owner().move_and_collide(Vector3(0,-1, 0))
 	
 	var temp_velocity = velocity
 	temp_velocity.y = 0
@@ -119,10 +121,9 @@ func walk(delta):
 		velocity.y = JUMP_HEIGHT
 		
 	#move
-	velocity = $"../../../KinematicBody".move_and_slide(velocity, Vector3(0,1,0))
+	velocity = get_owner().move_and_slide(velocity, Vector3(0,1,0))
 		
 	if Input.is_action_pressed("move_squat"):
-		$"../../PlayerCamera".transform(0, -2, 0)
-		$"../../PlayerCamera".translate(0, -2, 0)
+		playerCamera.transform(0, -2, 0)
 		
 		
