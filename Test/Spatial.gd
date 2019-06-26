fbf0e9a6e9b8b5c96e562364b23dad6036b1ff5d
Brendan Weirtz <brendanweirtz@gmail.com>
2019-06-26 23:47:07 +0000

    Replace Spatial.gd

--- Test/Spatial.gd
+++ Test/Spatial.gd
@@ -7,11 +7,13 @@ var mouse_toggle = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	print("Remove GUI from player node")
+	print("Add input restart.")
+
 	
 func _process(delta):
 	if (Input.is_action_just_pressed("ui_cancel")):
 		mouse_toggle()
-		
 		
 	if (Input.is_action_just_pressed("restart")):
 		get_tree().reload_current_scene()
@@ -22,12 +24,13 @@ func mouse_toggle():
 	if mouse_toggle == true:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		mouse_toggle = false
-		$KinematicBody/GUI/pause.show()
+		get_node("KinematicBody/GUI/pause").show()
+
 
 	elif mouse_toggle == false:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		mouse_toggle = true
-		$KinematicBody/GUI/pause.hide()
+		get_node("KinematicBody/GUI/pause").hide()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
