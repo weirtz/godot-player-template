8cce66b314e075f980bfbc5bef9c1e4a19d34413
Brendan Weirtz <brendanweirtz@gmail.com>
2019-06-26 23:46:18 +0000

    Replace console.gd

--- Test/scripts/player/console.gd
+++ Test/scripts/player/console.gd
@@ -1,23 +1,28 @@
 extends Control
 class_name Console
 
+
+
 func console():
 	$text_onfloor.add_text("false")
 	$text_fps.set_text("FPS: " + str(Engine.get_frames_per_second()))
 	
-	if $"../../PlayerMovement/Fly".flying == false:
-		if $"../../PlayerMovement/Movement".running == true:
+	var playerMove = get_owner().get_node("PlayerMovement/Movement")
+	var playerFly = get_owner().get_node("Playermovement/Fly")
+
+	if playerFly.flying == false:
+		if playerMove.running == true:
 			$text_movement.set_text("Movement: Running")
 		else:
-			if $"../../PlayerMovement/Movement".standing == false: 
+			if playerMove.standing == false: 
 				$text_movement.set_text("Movement: Walking")
 			else:
 				$text_movement.set_text("Movement: Standing")
 	else:
 		$text_movement.set_text("Movement: Flying")
 		
-	$text_fov.set_text("FOV: " + str($"../../PlayerCamera".fov))
-	$text_contact.set_text("Has contact(RayCast): " + str($"../../PlayerMovement/Movement".rayCast))
-	$text_onfloor.set_text("Is on floor: " + str($"../../PlayerMovement/Movement".on_floor))
-	$text_speed.set_text("Speed: " + str($"../../PlayerMovement/Movement".speed))
+	$text_fov.set_text("FOV: " + str(get_owner().get_node("PlayerCamera").fov))
+	$text_contact.set_text("Has contact(RayCast): " + str(playerMove.rayCast))
+	$text_onfloor.set_text("Is on floor: " + str(playerMove.on_floor))
+	$text_speed.set_text("Speed: " + str(playerMove.speed))
 	
